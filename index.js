26a33d0c8546d8a5d6c194dea4399790e7ed4356
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-11 17:27:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const botconfig = require("./botconfig.json");
 const bot = new Discord.Client({disableEveryone: true});
-
+const { QueryType } = require('discord-player');
+var pathToFfmpeg = require('ffmpeg-static');
 let botname = "CrBot"
 
 bot.on("ready", async() => {
@@ -34,7 +35,77 @@ bot.on("message", async message => {
  if(cmd === `${prefix}help`){
     message.channel.send("Parancsok : Hamarosan..., Készítő: Ádám#9999 , A bot hostingja: https://dashboard.heroku.com/apps , ");
 }
-    
+ 
+  
+
+module.exports = {
+    name: 'play',
+    aliases: ['p'],
+    utilisation: '{prefix}play [zene neve/linkje]',
+    voiceChannel: true,
+
+    async execute(client, message, args) {
+        if (!args[0]) return message.channel.send(`Mérlek adj meg egy érvényes keresést ${message.author}... Próbáld újra❌`);
+
+        const res = await player.search(args.join(' '), {
+            requestedBy: message.member,
+            searchEngine: QueryType.AUTO
+        });
+
+        if (!res || !res.tracks.length) return message.channel.send(`Nincs találat ${message.author}... Próbáld újra❌`);
+
+        const queue = await player.createQueue(message.guild, {
+            metadata: message.channel
+        });
+
+        try {
+            if (!queue.connection) await queue.connect(message.member.voice.channel);
+        } catch {
+            await player.deleteQueue(message.guild.id);
+            return message.channel.send(`Nem vagy hangcsatornában ${message.author}...  ❌`);
+        }
+
+        await message.channel.send(`Loading your ${res.playlist ? 'playlist' : 'track'}... 🎧`);
+
+        res.playlist ? queue.addTracks(res.tracks) : queue.addTrack(res.tracks[0]);
+
+        if (!queue.playing) await queue.play();
+    },
+};  
+
+module.exports = {
+    name: 'pause',
+    aliases: [],
+    utilisation: '{prefix}pause',
+    voiceChannel: true,
+
+    execute(client, message) {
+        const queue = player.getQueue(message.guild.id);
+
+        if (!queue) return message.channel.send(`Nincs zend${message.author}...❌`);
+
+        const success = queue.setPaused(true);
+
+        return message.channel.send(success ? `Jelenlegi zene megállítva ${queue.current.title}  ✅` : `Hiba történt ${message.author}... Kérlek próbáld újra ❌`);
+    },
+};
+
+module.exports = {
+    name: 'skip',
+    aliases: ['sk'],
+    utilisation: '{prefix}skip',
+    voiceChannel: true,
+
+    execute(client, message) {
+        const queue = player.getQueue(message.guild.id);
+
+        if (!queue || !queue.playing) return message.channel.send(`Nincs zene ❌`);
+
+        const success = queue.skip();
+
+        return message.channel.send(success ? `Zene: ${queue.current.title} átugorva ✅`);
+    },
+};
     
 })
 bot.login(process.env.TOKEN)
